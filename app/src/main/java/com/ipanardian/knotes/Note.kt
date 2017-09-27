012ab80f4c1e3c703bbca8b1cf85ba5a019f2d2c
ipanardian <ipan.software@gmail.com>
2017-09-27 22:27:29 +0700

    Change to data class

--- app/src/main/java/com/ipanardian/knotes/Note.kt
+++ app/src/main/java/com/ipanardian/knotes/Note.kt
@@ -4,14 +4,4 @@ package com.ipanardian.knotes
  * Created by ipan on 9/15/17.
  */
 
-class Note {
-    var id: Int? = null
-    var title: String? = null
-    var description: String? = null
-
-    constructor(id:Int, title:String, description:String) {
-        this.id = id
-        this.title = title
-        this.description = description
-    }
-}
+data class Note(var id: Int?, var title: String?, var description: String?)
